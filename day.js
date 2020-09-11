711fb6592d0b5d55419190f42e05e5a9e2941abe
akitaken <cat@catr.co>
2020-09-11 16:40:00 +1000

    Create js scripts

---
+++ day.js
@@ -0,0 +1,14 @@
+// Returns the day of the week for the given date
+function dayName(date) {
+  const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"];
+  return daysOfTheWeek[date.getDay()];
+}
+
+// Returns a greeting for the given date.
+function greeting(date) {
+  // FILL IN
+}
+
+function greeting(time) {
+  return `Hello, world! Happy ${dayName(time)}!`;
+}
