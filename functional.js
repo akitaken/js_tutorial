16095fd1f2ca80ca02710579c1be6b777bfa8bd1
akitaken <cat@catr.co>
2020-09-11 22:57:08 +1000

    Complete Ch6 Funtional Programming

--- functional.js
+++ functional.js
@@ -6,6 +6,8 @@ function urlify(string) {
     return string.toLowerCase().split(/\s+/).join("-");
 }
 
+// MAP
+
 // urls: Imperative version
 function imperativeUrls(elements) {
   let urls = [];
@@ -31,3 +33,82 @@ function urlArray(elements) {
   return elements.map(element => "https://example.com/" + urlify(element) );
 }
 console.log(urlArray(states));
+
+
+// FILTER
+
+// singles: Imperative version
+function imperativeSingles(elements) {
+  let singles = [];
+  elements.forEach(function(element) {
+    if (element.split(/\s+/).length === 1) {
+      singles.push(element);
+    }
+  })
+  return singles;
+}
+console.log(imperativeSingles(states));
+
+// singles: Functional version:
+function functionalSingles(elements) {
+  return elements.filter(element => element.split(/\s+/).length === 1);
+}
+console.log(functionalSingles(states));
+
+
+// check Dakota: test for the presence of the string “Dakota”
+function checkDakota(elements) {
+  return elements.filter(element => element.includes("Dakota"));
+}
+console.log(checkDakota(states));
+
+// check Dakota: Regex test for length === 2
+function functionalDoubles(elements) {
+  return elements.filter(element => element.split(/\s+/).length === 2);
+}
+console.log(functionalDoubles(states));
+
+// REDUCE
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// sum: Imperative Solution
+function imperativeSum(elements) {
+  let total = 0;
+  elements.forEach(function (n) {
+    total += n;
+  });
+  return total;
+}
+console.log(imperativeSum(numbers));
+
+function functionalSum(elements) {
+  return elements.reduce((total, n) => {return total +=n});
+}
+console.log(functionalSum(numbers));
+
+
+// product: functional Solution
+function functionalProduct(elements) {
+  return elements.reduce((total, n) => {return total *=n});
+}
+console.log(functionalProduct(numbers));
+
+// lengths: Imperative Solution
+function imperativeLengths(elements) {
+  let lengths = {};
+  elements.forEach(function(element) {
+    lengths[element] = element.length;
+  });
+  return lengths;
+}
+console.log(imperativeLengths(states));
+
+// lengths functional Solution
+function functionalLengths(elements) {
+  return elements.reduce((lengths, element) => {
+    lengths[element] = element.length;
+    return lengths;
+  }, {});
+}
+console.log(functionalLengths(states));
